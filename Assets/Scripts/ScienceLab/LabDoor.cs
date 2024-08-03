940cfd57384479dc72e7887ba0fc148d3ffbc923
Nathan Blore <wizbe@Nathans-MacBook-Pro.local>
2024-08-03 01:10:01 +0100

    Adding cave tiles

--- Assets/Scripts/ScienceLab/LabDoor.cs
+++ Assets/Scripts/ScienceLab/LabDoor.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class LabDoor : MonoBehaviour
 {
-public Animator animator;
+    public Animator animator;
+
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("Player")) {
+        if(other.CompareTag("Player") && animator.isActiveAndEnabled) {
             animator.Play("Open");
         }
     }
 
-     private void OnTriggerExit2D(Collider2D other) {
-        if(other.CompareTag("Player")) {
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Player") && animator.isActiveAndEnabled) {
             animator.Play("Close");
         }
     }
