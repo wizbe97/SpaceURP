5a948a64c2f4214fd50547cc5eb969292139df1f
wizbe97 <nathanblore97@gmail.com>
2024-09-30 00:20:43 +0100

    Almost working spear attack

--- Assets/Scripts/Weapons/SpearAttack.cs
+++ Assets/Scripts/Weapons/SpearAttack.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class SpearAttack : MonoBehaviour
 {
     private Animator animator;
-    private SpriteRenderer spriteRenderer;
+    private PolygonCollider2D polygonCollider;
     void Start()
     {
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        polygonCollider = GetComponentInChildren<PolygonCollider2D>();
     }
 
     public void Attack(float attackDirectionX, float attackDirectionY)
     {
         Debug.Log("Attacking with spear");
+        polygonCollider.enabled = true;
         animator.SetBool("isAttacking", true);
         animator.SetFloat("mouseX", attackDirectionX);
         animator.SetFloat("mouseY", attackDirectionY);
@@ -23,5 +24,6 @@ public class SpearAttack : MonoBehaviour
     public void AttackEnd()
     {
         animator.SetBool("isAttacking", false);
+        polygonCollider.enabled = false;
     }
 }
---
+++ Assets/Scripts/Weapons/WeaponCollider.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCollider : MonoBehaviour
+{
+    private Coroutine damageCoroutine;
+    private WeaponInfo weaponInfo;
+
+    private void Start() {
+        weaponInfo = GetComponent<WeaponInfo>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyCharacter enemy = other.gameObject.GetComponent<EnemyCharacter>();
+            Rigidbody2D enemyRb = other.gameObject.GetComponent<Rigidbody2D>();
+
+            // Apply knockback
+            Vector2 knockbackDirection = (other.transform.position - transform.position).normalized;
+            enemyRb.AddForce(knockbackDirection * weaponInfo.knockbackForce, ForceMode2D.Impulse);
+
+            // Only call DamageCharacter on the Enemy if we don't currently have a DamageCharacter() Coroutine running.
+            if (damageCoroutine == null)
+            {
+                damageCoroutine = StartCoroutine(enemy.DamageCharacter(weaponInfo.damage, 0f));
+            }
+        }
+    }
+}
